235c998bfa0113f373c989022c15a482df123b14
AndreiBurak <59471505+AndreiBurak@users.noreply.github.com>
2024-10-08 07:20:40 +0200

    Create powerschell.sh

---
+++ powerschell.sh
@@ -0,0 +1,18 @@
+# Get the version of Ubuntu
+source /etc/os-release
+
+# Download the Microsoft repository keys
+wget -q https://packages.microsoft.com/config/ubuntu/$VERSION_ID/packages-microsoft-prod.deb
+
+# Register the Microsoft repository keys
+sudo dpkg -i packages-microsoft-prod.deb
+
+# Delete the Microsoft repository keys file
+rm packages-microsoft-prod.deb
+
+# Update the list of packages after we added packages.microsoft.com
+sudo apt-get update
+
+###################################
+# Install PowerShell
+sudo apt-get install -y powershell

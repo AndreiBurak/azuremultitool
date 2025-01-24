edcbeccc1cdfcd3831a1c7a832f3042fc93019ec
Andrei Burak <a3.burak@itransition.com>
2025-01-24 15:19:55 +0100

    Initial commit

--- scripts/powershell.sh
+++ scripts/powershell.sh
@@ -1,18 +1,13 @@
 # Get the version of Ubuntu
 source /etc/os-release
-
 # Download the Microsoft repository keys
 wget -q https://packages.microsoft.com/config/ubuntu/$VERSION_ID/packages-microsoft-prod.deb
-
 # Register the Microsoft repository keys
 dpkg -i packages-microsoft-prod.deb
-
-# Delete the Microsoft repository keys file
+#Delete the Microsoft repository keys file
 rm packages-microsoft-prod.deb
-
 # Update the list of packages after we added packages.microsoft.com
 apt-get update
-
 ###################################
 # Install PowerShell
 apt-get install -y powershell
